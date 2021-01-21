0e5971aac121cf53b7d833c8bc95e5126eafd03c
sauld <sauld@DESKTOP-S3F0FRM>
2021-01-21 20:49:19 +0000

    Kata1 Final

---
+++ src/kata1/Kata1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata1;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+/**
+ *
+ * @author sauld
+ */
+public class Kata1 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Person person = new Person("Saúl", LocalDate.of(2000, Month.OCTOBER,22)); 
+        System.out.println(person.getName() + " " + person.getAge());
+    }
+}
---
+++ src/kata1/Person.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata1;
+
+/**
+ *
+ * @author sauld
+ */
+import java.time.LocalDate;
+
+public class Person {
+    private final String name;
+    private final LocalDate birthDate; 
+
+    public Person(String name, LocalDate birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;   
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+    
+    public int getAge(){
+        return toYears(LocalDate.now().toEpochDay() - this.birthDate.toEpochDay());
+    }
+    
+    private int toYears(long days){
+        return (int)(days/365.25);
+    }
+}
